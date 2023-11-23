e6066f50b84b75f81f2ba4dbc41450418762f235
Ryobia <ryobia36@gmail.com>
2023-11-23 11:24:10 -0700

    added logos

--- client/src/pages/WhatToAd.js
+++ client/src/pages/WhatToAd.js
@@ -1,34 +1,41 @@
 import React from "react";
+import targetLogo from "../../src/images/target.png";
+import walmartLogo from "../../src/images/walmart.jpg";
+import amazonLogo from "../../src/images/amazon.png";
 import { Link } from "react-router-dom";
 
 const WhatToAd = () => {
   return (
     <section className="whatToAddList">
-      
-        <a className="whatToAddBtn" href="https://www.target.com/">
-          <div className="">
-            <h1>TARGET</h1>
-          </div>
-        </a>
+      <a className="whatToAddBtn" href="">
+        <div className="">
+          <h1>Your Store/Website Here!</h1>
+        </div>
+      </a>
 
-        <a className="whatToAddBtn" href="https://www.amazon.com/ref=nav_logo">
-          <div className="">
-            <h1>Good 'ol Amazon</h1>
-          </div>
-        </a>
+      <a
+        className="whatToAddBtn"
+        href="https://www.target.com/"
+        target="_blank"
+      >
+        <img height="100%" width="50%" src={targetLogo} />
+      </a>
 
-        <a className="whatToAddBtn" href="https://www.walmart.com/">
-          <div className="">
-            <h1>Wally World</h1>
-          </div>
-        </a>
+      <a
+        className="whatToAddBtn"
+        href="https://www.amazon.com/ref=nav_logo"
+        target="_blank"
+      >
+        <img height="100%" width="70%" src={amazonLogo} />
+      </a>
 
-        <a className="whatToAddBtn" href="">
-          <div className="">
-            <h1>Your Store/Website Here!</h1>
-          </div>
-        </a>
-      
+      <a
+        className="whatToAddBtn"
+        href="https://www.walmart.com/"
+        target="_blank"
+      >
+        <img height="100%" width="50%" src={walmartLogo} />
+      </a>
     </section>
   );
 };
